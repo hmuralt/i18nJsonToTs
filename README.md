aeb5942f1f02fc3a835f1641b301f6613affe5b7
Heinrich Muralt <heinrich@supertext.ch>
2019-04-06 13:38:29 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# i18nJsonToTs
+
+Tool to generate TypeScript out of I18N JSON strings.
+
+Note: this is not a one to one mapping from JSON to TypeScript.
---
+++ jest.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  transform: {
+    "^.+\\.tsx?$": "ts-jest"
+  },
+  testRegex: "/tests/.*\\.test\\.tsx?$",
+  moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"]
+};
