acbf078e086ec8d94f6f17e7e72b8c0e49e88818
Heinrich Muralt <heinrich@supertext.ch>
2019-04-07 19:13:14 +0200

    Added tests for none string conversion

--- tests/JsonConvertion.test.ts
+++ tests/JsonConvertion.test.ts
@@ -21,5 +21,45 @@ describe("JsonConvertion", () => {
         });
       });
     });
+
+    describe("when converting number property", () => {
+      it("converts property to NoneStringValueDescription", () => {
+        // Arrange
+        const testPropertyName = "myNumberProp";
+        const testPropertyValue = 23662;
+        const testJsonObject = {
+          [testPropertyName]: testPropertyValue
+        };
+
+        // Act
+        const result = convertJson(JSON.stringify(testJsonObject));
+
+        // Assert
+        expect(result.propertyDescriptions[0].key).toBe(testPropertyName);
+        expect(result.propertyDescriptions[0].valueDescription).toEqual({
+          value: testPropertyValue
+        });
+      });
+    });
+
+    describe("when converting array property", () => {
+      it("converts property to NoneStringValueDescription", () => {
+        // Arrange
+        const testPropertyName = "myArrayProp";
+        const testPropertyValue = [3, 7, 4, 8];
+        const testJsonObject = {
+          [testPropertyName]: testPropertyValue
+        };
+
+        // Act
+        const result = convertJson(JSON.stringify(testJsonObject));
+
+        // Assert
+        expect(result.propertyDescriptions[0].key).toBe(testPropertyName);
+        expect(result.propertyDescriptions[0].valueDescription).toEqual({
+          value: testPropertyValue
+        });
+      });
+    });
   });
 });
