87a29055f36bc0b634f2826f58cb791928a361a7
Heinrich Muralt <heinrich@supertext.ch>
2019-05-17 15:55:53 +0200

    Added default export option

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,19 @@
-import { createSourceFile, ScriptTarget, ScriptKind, createPrinter, EmitHint } from "typescript";
+import { createSourceFile, ScriptTarget, ScriptKind, createPrinter, EmitHint, createExportDefault } from "typescript";
 import convertObject from "./JsonToIntermediate/JsonConversion";
 import createExpression from "./IntermediateToTypeScript/ExpressionCreation";
 
-export function getTypeScriptFromString(i18nJson: string) {
+export function getTypeScriptFromString(i18nJson: string, isDefaultExport: boolean = false) {
   return getTypeScriptFromObject(JSON.parse(i18nJson));
 }
 
-export function getTypeScriptFromObject(i18nJson: {}) {
+export function getTypeScriptFromObject(i18nJson: {}, isDefaultExport: boolean = false) {
   const intermediate = convertObject(i18nJson);
   const typeScriptAst = createExpression(intermediate);
   const resultFile = createSourceFile("notExisting.ts", "", ScriptTarget.Latest, false, ScriptKind.TS);
   const printer = createPrinter();
-  return printer.printNode(EmitHint.Unspecified, typeScriptAst, resultFile);
+  return printer.printNode(
+    EmitHint.Unspecified,
+    isDefaultExport ? createExportDefault(typeScriptAst) : typeScriptAst,
+    resultFile
+  );
 }
